ad6661bdb8db95a68d370dfcf2e6b31b3f2cc6f5
Francisco Marshall <fcoarturo.marshall@gmail.com>
2023-03-18 11:22:31 -0300

    added model scale

--- src/script.js
+++ src/script.js
@@ -13,11 +13,11 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-camera.position.z = 0.5;
-camera.position.y = 1.5;
+camera.position.z = 2;
+camera.position.y = 0.3;
 camera.position.x = 1;
 
-const renderer = new THREE.WebGLRenderer({ alpha: true });
+const renderer = new THREE.WebGLRenderer({ antialias:true, alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0x000000, 0); // transparent background
 
@@ -31,6 +31,7 @@ controls.screenSpacePanning = false;
 controls.minDistance = 1;
 controls.maxDistance = 100;
 
+//LIGHT
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
 scene.add(ambientLight);
 
@@ -38,12 +39,20 @@ const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
 directionalLight.position.set(0, 2, 1).normalize();
 scene.add(directionalLight);
 
+
+// MODEL LOAD
 let mixer;
 
 const loader = new GLTFLoader();
 loader.load("./KyonConAnimaciones2.gltf", function (gltf) {
   scene.add(gltf.scene);
   mixer = new THREE.AnimationMixer(gltf.scene);
+
+// size & position
+  gltf.scene.position.y = -1;
+  gltf.scene.scale.set(1.3,1.3,1.3);
+
+  //animation
   const action = mixer.clipAction(gltf.animations[0]);
   action.play();
   renderer.render(scene,camera);
